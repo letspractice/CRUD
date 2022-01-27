52d72a5c0cf3eec5ea911ff8a12e012f35249342
letspractice <98416202+letspractice@users.noreply.github.com>
2022-01-28 02:22:48 +0530

    Add files via upload

---
+++ EFDbContext/EFDbContext/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using EFDbContext.Data;
+using EFDbContext.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFDbContext.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger,ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var tickets = await _context.TravelTicket.ToListAsync();
+            //var tickets = await _context.TravelTicket.ToListAsync();
+            return View(tickets);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EFDbContext/EFDbContext/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EFDbContext.Models;
+using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFDbContext.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<TravelTicket> TravelTicket { get; set; }
+    }
+}
